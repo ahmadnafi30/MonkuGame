ac0d0490fb70c58d8eadcf05a179aa8e02fca02a
raka1231 <rakafadillah123@gmail.com>
2024-05-18 23:16:45 +0700

    pake html aowkawok

--- src/gui/gui/HomeBase.java
+++ src/gui/gui/HomeBase.java
@@ -1,17 +1,19 @@
 package gui;
+
 import javax.swing.*;
-
-import org.w3c.dom.events.MouseEvent;
-
 import java.awt.*;
-import java.util.Collections;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Collections;
 
-public class HomeBase {
-    public HomeBase(){
+
+public class HomeBase extends JFrame implements ActionListener{
+    private CardLayout dialogText;
+    private JPanel dialogTextPanel;
+
+    public HomeBase() {
         JFrame frame = new JFrame("Monku Games");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 750);
@@ -41,9 +43,11 @@ public class HomeBase {
                 g.drawImage(originalImage, 0, 0, getWidth(), getHeight(), this);
             }
         };
-        professor.setBounds(70, 510, 298/2, 295/2);
+        professor.setBounds(70, 510, 298 / 2, 295 / 2);
         panelBG.add(professor);
-
+        dialogText = new CardLayout();
+        dialogTextPanel = new JPanel(dialogText);
+        
         JPanel dialogBox = new JPanel(null) {
             @Override
             protected void paintComponent(Graphics g) {
@@ -54,9 +58,45 @@ public class HomeBase {
             }
         };
         dialogBox.setBounds(3, 450, 980, 265);
+        dialogTextPanel.setBounds(80, 500, dialogBox.getWidth() - 160, 265);
+        dialogTextPanel.setOpaque(false);
+        createDialogCard("<html><p style=\"margin-left: 20px\">Halo Kamu!</p>Siapa namamu?</html>");
+        createDialogCard("Ini adalah dialog 2");
+        createDialogCard("Ini adalah dialog 3");
+        panelBG.add(dialogTextPanel);
         panelBG.add(dialogBox);
+
+
+        // Create dialog cards
+
+
+        // Create an invisible button to capture clicks and switch dialogs
+        JButton invisibleButton = new JButton();
+        invisibleButton.setBounds(dialogBox.getBounds());
+        invisibleButton.setOpaque(false);
+        invisibleButton.setContentAreaFilled(false);
+        invisibleButton.setBorderPainted(false);
+        invisibleButton.addActionListener(e -> dialogText.next(dialogTextPanel));
+        
+        panelBG.add(invisibleButton);
+
+        frame.setVisible(true);
     }
 
+    private void createDialogCard(String text) {
+        JPanel panel = new JPanel();
+        panel.setOpaque(false);
+        JLabel label = new JLabel(text, SwingConstants.CENTER);
+        label.setFont(new Font("Purisa Bold", Font.BOLD, 30));
+        label.setForeground(Color.BLACK); // Set text color
+        label.setVisible(true);
+        panel.add(label);
+        dialogTextPanel.add(panel);
+    }
 
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 }
