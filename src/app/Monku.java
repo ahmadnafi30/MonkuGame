3161df52c34d79fafa695b4abbcbc0a53df49f7a
raka1231 <rakafadillah123@gmail.com>
2024-05-27 22:44:38 +0700

    Wsssssssss

--- src/app/Monku.java
+++ src/app/Monku.java
@@ -57,6 +57,7 @@ public class Monku {
     public static Locations doomDungeon = new Dungeon("Temple of Doom", monsters5, items, 5, "asset/Dungeon/Temple of Doom lvl5/bg-dampcave.jpg", "asset/Dungeon/Temple of Doom lvl5/cueball-gen1rb.png", "Cueball", "asset/Music/Bicycle.wav\"");
 
     public static void main(String[] args) throws IOException {
+        //player.setCoin(99999);
         String[] skillsAirName = {
             "Gust",
             "Air Slash",
